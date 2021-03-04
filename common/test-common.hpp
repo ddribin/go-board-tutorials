39da4f9aa185472826093f931e73fa62eed4d6ff
Dave Dribin <dave-vcs@dribin.org>
2021-03-04 10:48:03 -0600

    Add code and tests for project-01

---
+++ common/test-common.hpp
@@ -0,0 +1,8 @@
+#if __INTELLISENSE__
+#pragma diag_suppress 2486
+#endif
+
+#include <verilated.h>
+
+// This must be last so any "operator <<"" overloads work properly for test failures
+#include <catch2/catch.hpp>
---
+++ project-01-led/test-led.cpp
@@ -0,0 +1,29 @@
+
+#include "test-common.hpp"
+#include "VSwitches_To_LEDs.h"
+
+TEST_CASE("Initial state")
+{
+    VSwitches_To_LEDs core;
+
+    core.eval();
+
+    REQUIRE(core.o_LED_1 == 0);
+    REQUIRE(core.o_LED_2 == 0);
+    REQUIRE(core.o_LED_3 == 0);
+    REQUIRE(core.o_LED_4 == 0);
+}
+
+TEST_CASE("Test change switches")
+{
+    VSwitches_To_LEDs core;
+    core.i_Switch_1 = 1;
+    core.i_Switch_3 = 1;
+    
+    core.eval();
+
+    REQUIRE(core.o_LED_1 == 1);
+    REQUIRE(core.o_LED_2 == 0);
+    REQUIRE(core.o_LED_3 == 1);
+    REQUIRE(core.o_LED_4 == 0);
+}
