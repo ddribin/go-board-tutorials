0155a01cfd6208ccef2f74f8ff10524e79d48ccc
Dave Dribin <dave-vcs@dribin.org>
2021-03-22 00:02:06 -0500

    project-03-flip-flop: Add text fixture.
    Remove Clocked_Logic_Intro.hpp

--- project-03-flip-flop/test-flip-flop.cpp
+++ project-03-flip-flop/test-flip-flop.cpp
@@ -8,6 +8,23 @@ static const uint8_t SwitchUp = 0;
 static const uint8_t LedOff = 0;
 static const uint8_t LedOn = 1;
 
+struct ClockedLogicFixture {
+    using SignalPublisher8 = SignalPublisher<uint8_t, UUT>;
+    using SignalObserver8 = SignalObserver<uint8_t, UUT>;
+    TestBench<UUT> bench;
+    SignalPublisher8 switchInput;
+    SignalObserver8 ledOutput;
+
+    ClockedLogicFixture() :
+        switchInput(&UUT::i_Switch_1),
+        ledOutput(&UUT::o_LED_1)
+    {
+        bench.addPreHook(switchInput.hook());
+        bench.addPostHook(ledOutput.hook());
+    }
+};
+using Fixture = ClockedLogicFixture;
+
 TEST_CASE("[flip-flop] Initial state", "[project-03]")
 {
     UUT core;
@@ -17,49 +34,36 @@ TEST_CASE("[flip-flop] Initial state", "[project-03]")
     REQUIRE(core.o_LED_1 == LedOff);
 }
 
-TEST_CASE("[flip-flop] Switch down", "[project-03]")
+TEST_CASE_METHOD(Fixture, "[flip-flop] Switch down", "[project-03]")
 {
-    TestBench<UUT> bench;
-    auto switchPublisher = makePublisher(&UUT::i_Switch_1);
-    switchPublisher.addInputs({{1, SwitchDown}});
-    auto ledObserver = makeObserver(&UUT::o_LED_1);
+    switchInput.addInputs({{1, SwitchDown}});
 
     bench.tick(10);
 
     ChangeVector8 expected;
-    REQUIRE(ledObserver.changes() == expected);
+    REQUIRE(ledOutput.changes() == expected);
 }
 
-TEST_CASE("[flip-flop] Switch down then up", "[project-03]")
+TEST_CASE_METHOD(Fixture, "[flip-flop] Switch down then up", "[project-03]")
 {
-    TestBench<UUT> bench;
-    auto switchPublisher = makePublisher(&UUT::i_Switch_1);
-    switchPublisher.addInputs({{1, SwitchDown}, {2, SwitchUp}});
-    bench.addPreHook(switchPublisher.hook());
-
-    auto ledObserver = makeObserver(&UUT::o_LED_1);
-    bench.addPostHook(ledObserver.hook());
+    switchInput.addInputs({{1, SwitchDown}, {2, SwitchUp}});
 
     bench.tick(10);
 
     ChangeVector8 expected{{2, LedOn}};
-    REQUIRE(ledObserver.changes() == expected);
+    REQUIRE(ledOutput.changes() == expected);
 }
 
-TEST_CASE("[flip-flop] Switch toggle 3 times", "[project-03]")
+TEST_CASE_METHOD(Fixture, "[flip-flop] Switch toggle 3 times", "[project-03]")
 {
-    TestBench<UUT> bench;
-    auto switchPublisher = makePublisher(&UUT::i_Switch_1, {
+    switchInput.addInputs({
         {1, SwitchDown}, {2,  SwitchUp},
         {4, SwitchDown}, {6,  SwitchUp},
         {8, SwitchDown}, {11, SwitchUp}
     });
-    bench.addPreHook(switchPublisher.hook());
-    auto ledObserver = makeObserver(&UUT::o_LED_1);
-    bench.addPostHook(ledObserver.hook());
     
     bench.tick(15);
 
     ChangeVector8 expected{{2, LedOn}, {6, LedOff}, {11, LedOn}};
-    REQUIRE(ledObserver.changes() == expected);
+    REQUIRE(ledOutput.changes() == expected);
 }
