71cd0beb7c9ca52dbaf0fe7ffc84baa6ceedc0ec
Dave Dribin <dave-vcs@dribin.org>
2021-04-24 16:35:24 -0500

    project-08a-uart-tx: Tests are passing

--- project-08a-uart-tx/UART_Transmitter.v
+++ project-08a-uart-tx/UART_Transmitter.v
@@ -8,5 +8,82 @@ module UART_Transmitter #(
   output        o_tx_active,
   output        o_tx_done
 );
+
+  localparam STATE_INIT       = 2'd0;
+  localparam STATE_START_BIT  = 2'd1;
+  localparam STATE_DATA_BITS  = 2'd2;
+  localparam STATE_STOP_BIT   = 2'd3;
+
+  localparam COUNT_WIDTH = $clog2(CYCLES_PER_BIT);
+
+  reg [7:0]     r_tx_byte;
+  reg           r_tx_serial;
+  reg           r_tx_active;
+  reg           r_tx_done;
+
+  reg [1:0]     r_state;
+  reg [COUNT_WIDTH-1:0] r_count = 0;
+  reg [2:0]   r_bit_count = 0;
+
+  always @(posedge i_clk) begin
+    case (r_state)
+      STATE_INIT : begin
+        r_tx_done <= 0;
+        if (i_tx_dv) begin
+          r_state <= STATE_START_BIT;
+          r_tx_byte <= i_tx_byte;
+          r_tx_serial <= 0;
+          r_tx_active <= 1;
+          r_count <= 0;
+          r_bit_count <= 0;
+        end else begin
+          r_tx_active <= 0;
+          r_tx_serial <= 1;
+        end
+      end
+
+      STATE_START_BIT : begin
+        if (r_count == CYCLES_PER_BIT-1) begin
+          r_state <= STATE_DATA_BITS;
+          r_tx_serial <= r_tx_byte[0];
+          r_tx_byte <= r_tx_byte >> 1;
+          r_count <= 0;
+        end else begin
+          r_count <= r_count + 1;
+        end
+      end
+
+      STATE_DATA_BITS : begin
+        if (r_count == CYCLES_PER_BIT-1) begin
+          r_count <= 0;
+          if (r_bit_count == 7) begin
+            r_state <= STATE_STOP_BIT;
+            r_tx_serial <= 1;
+          end else begin
+            r_tx_serial <= r_tx_byte[0];
+            r_tx_byte <= r_tx_byte >> 1;
+            r_bit_count <= r_bit_count + 1;
+          end
+        end else begin
+          r_count <= r_count + 1;
+        end
+      end
+
+      STATE_STOP_BIT : begin
+        if (r_count == CYCLES_PER_BIT-2) begin
+          r_tx_done <= 1;
+          r_state <= STATE_INIT;
+        end else begin
+          r_count <= r_count + 1;
+        end
+      end
+
+      default: r_state <= STATE_INIT;
+    endcase
+  end
+
+  assign o_tx_serial = r_tx_serial;
+  assign o_tx_active = r_tx_active;
+  assign o_tx_done = r_tx_done;
   
 endmodule
--- project-08a-uart-tx/test-uart-transmitter.cpp
+++ project-08a-uart-tx/test-uart-transmitter.cpp
@@ -41,3 +41,137 @@ TEST_CASE_METHOD(Fixture, "Initial state", "[project-08a]")
     REQUIRE(core.o_tx_active == 0);
     REQUIRE(core.o_tx_done == 0);
 }
+
+TEST_CASE_METHOD(Fixture, "Transmit 1 byte", "[project-08a]")
+{
+    bench.openTrace("/tmp/tx-1-byte.vcd");
+    // Send 0x37 = 0011 0111
+    transmitByte.addInputs({{10, 0x37}, {11, 0x00}});
+    transmitValid.addInputs({{10, 1}, {11, 0}});
+
+    bench.tick(150);
+
+    ChangeVector8 expectedActive({{10, 1}, {110, 0}});
+    CHECK(transmitActive.changes() == expectedActive);
+
+    ChangeVector8 expectedDone({{109, 1}, {110, 0}});
+    CHECK(transmitDone.changes() == expectedDone);
+
+    ChangeVector8 expectedSerial({
+        {1,   1},   // Initial pull-up
+        {10,  0},   // Start bit
+        {20,  1},   // Bit 0
+    //  {30,  1},   // Bit 1
+    //  {40,  1},   // Bit 2
+        {50,  0},   // Bit 3
+        {60,  1},   // Bit 4
+    //  {70,  1},   // Bit 5
+        {80,  0},   // Bit 6
+    //  {90,  0},   // Bit 7
+        {100, 1},   // Stop bit
+    });
+    CHECK(transmitSerial.changes() == expectedSerial);
+}
+
+TEST_CASE_METHOD(Fixture, "Transmit 2 bytes with delay", "[project-08a]")
+{
+    bench.openTrace("/tmp/tx-2-delay.vcd");
+    // Send 0x37 = 0011 0111
+    transmitByte.addInputs({{10, 0x37}, {11, 0x00}});
+    transmitValid.addInputs({{10, 1}, {11, 0}});
+    // Send 0x90 = 1001 0000
+    transmitByte.addInputs({{210, 0x90}, {211, 0x00}});
+    transmitValid.addInputs({{210, 1}, {211, 0}});
+
+    bench.tick(350);
+
+    ChangeVector8 expectedActive({
+        {10,  1}, {110, 0},
+        {210, 1}, {310, 0},
+    });
+    CHECK(transmitActive.changes() == expectedActive);
+
+    ChangeVector8 expectedDone({
+        {109, 1}, {110, 0},
+        {309, 1}, {310, 0},
+    });
+    CHECK(transmitDone.changes() == expectedDone);
+
+    ChangeVector8 expectedSerial({
+        {1,   1},   // Initial pull-up
+        // Byte 1
+        {10,  0},   // Start bit
+        {20,  1},   // Bit 0
+    //  {30,  1},   // Bit 1
+    //  {40,  1},   // Bit 2
+        {50,  0},   // Bit 3
+        {60,  1},   // Bit 4
+    //  {70,  1},   // Bit 5
+        {80,  0},   // Bit 6
+    //  {90,  0},   // Bit 7
+        {100, 1},   // Stop bit
+        // Byte 2
+        {210, 0},   // Start bit
+    //  {220, 0},   // Bit 0
+    //  {230, 0},   // Bit 1
+    //  {240, 0},   // Bit 2
+    //  {250, 0},   // Bit 3
+        {260, 1},   // Bit 4
+        {270, 0},   // Bit 5
+    //  {280, 0},   // Bit 6
+        {290, 1},   // Bit 7
+    //  {300, 1},   // Stop bit
+    });
+    CHECK(transmitSerial.changes() == expectedSerial);
+}
+
+TEST_CASE_METHOD(Fixture, "Transmit 2 bytes back-to-back", "[project-08a]")
+{
+    bench.openTrace("/tmp/tx-2-back-to-back.vcd");
+    // Send 0x37 = 0011 0111
+    transmitByte.addInputs({{10, 0x37}, {11, 0x00}});
+    transmitValid.addInputs({{10, 1}, {11, 0}});
+    // Send 0x90 = 1001 0000
+    transmitByte.addInputs({{110, 0x90}, {111, 0x00}});
+    transmitValid.addInputs({{110, 1}, {111, 0}});
+
+    bench.tick(250);
+
+    ChangeVector8 expectedActive({
+        {10,  1}, {210, 0},
+    });
+    CHECK(transmitActive.changes() == expectedActive);
+
+    ChangeVector8 expectedDone({
+        {109, 1}, {110, 0},
+        {209, 1}, {210, 0},
+    });
+    CHECK(transmitDone.changes() == expectedDone);
+
+    ChangeVector8 expectedSerial({
+        {1,   1},   // Initial pull-up
+        // Byte 1
+        {10,  0},   // Start bit
+        {20,  1},   // Bit 0
+    //  {30,  1},   // Bit 1
+    //  {40,  1},   // Bit 2
+        {50,  0},   // Bit 3
+        {60,  1},   // Bit 4
+    //  {70,  1},   // Bit 5
+        {80,  0},   // Bit 6
+    //  {90,  0},   // Bit 7
+        {100, 1},   // Stop bit
+        // Byte 2
+        {110, 0},   // Start bit
+    //  {120, 0},   // Bit 0
+    //  {130, 0},   // Bit 1
+    //  {140, 0},   // Bit 2
+    //  {150, 0},   // Bit 3
+        {160, 1},   // Bit 4
+        {170, 0},   // Bit 5
+    //  {180, 0},   // Bit 6
+        {190, 1},   // Bit 7
+    //  {200, 1},   // Stop bit
+    });
+    CHECK(transmitSerial.changes() == expectedSerial);
+}
