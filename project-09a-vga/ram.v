48b2337ff69650c158e9cd6dbd91b72ab99541af
Dave Dribin <dave-vcs@dribin.org>
2021-05-12 00:23:57 -0500

    project-09a-vga: Use modern Verilog syntax in ram.v

--- project-09a-vga/ram.v
+++ project-09a-vga/ram.v
@@ -43,24 +43,50 @@
 //   defparam ram1024x4_inst.INIT_F = 256'h0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;
 // endmodule
 `elsif INFER
-module ram (din, addr, write_en, clk, dout);// 512x8
-  parameter addr_width = 10;
-  parameter data_width = 4;
-  input [addr_width-1:0] addr;
-  input [data_width-1:0] din;
-  input write_en, clk;
-  output [data_width-1:0] dout;
-  reg [data_width-1:0] dout; // Register for output.
-  reg [data_width-1:0] mem [(1<<addr_width)-1:0];
+// module ram (din, addr, write_en, clk, dout);// 512x8
+//   parameter addr_width = 10;
+//   parameter data_width = 4;
+//   input [addr_width-1:0] addr;
+//   input [data_width-1:0] din;
+//   input write_en, clk;
+//   output [data_width-1:0] dout;
+//   reg [data_width-1:0] dout; // Register for output.
+//   reg [data_width-1:0] mem [(1<<addr_width)-1:0];
 
-  always @(posedge clk)
-  begin
-    if (write_en) mem[(addr)] <= din;
-    dout <= mem[addr]; // Output register controlled by clock.
+//   always @(posedge clk)
+//   begin
+//     if (write_en) mem[(addr)] <= din;
+//     dout <= mem[addr]; // Output register controlled by clock.
+//   end
+
+//   initial begin
+//     $readmemh("foo.txt", mem);
+//   end
+// endmodule
+
+module ram #(
+  parameter ADDR_WIDTH = 10,
+  parameter DATA_WIDTH = 4
+) (
+  input clk,
+  input [DATA_WIDTH-1:0] din,
+  input [ADDR_WIDTH-1:0] addr,
+  input write_en,
+  output reg [DATA_WIDTH-1:0] dout
+);
+
+  reg [DATA_WIDTH-1:0] r_mem[(1<<ADDR_WIDTH) - 1:0];
+
+  always @(posedge clk) begin
+    if (write_en) begin
+      r_mem[addr] <= din;
+    end
+    dout = r_mem[addr];
   end
 
   initial begin
-    $readmemh("foo.txt", mem);
+    $readmemh("foo.txt", r_mem);
   end
 endmodule
+
 `endif
